132f8a6a4754c2a440fc2a69f2df4ec232e0eac6
Santiago Sanchez <chagostwo@yahoo.com>
2019-06-16 20:48:10 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -1,7 +1,8 @@
 require 'pry'
 class Song
   include Paramable
-  extend Memorable
+  extend Memorable::ClassMethods
+  include Memorable::InstanceMethods
   attr_accessor :name
   attr_reader :artist
 
